08aac64ddc57dece1aa47f252e640057849b28cb
hyunahprincess <aare1213@gmail.com>
2018-05-03 01:47:04 +0900

    dataStructure

---
+++ ch1_LinkedList/ch1_LinkedList/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  ch1_LinkedList
+//
+//  Created by Hyunah on 2018. 5. 3..
+//  Copyright © 2018년 hyunable. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int Data;   //데이터 필드
+    struct Node* NextNode;  //다음 노드를 가리키는 포인터
+}Node;
+
+
+
+int main() {
+    
+    
+    
+    /* 노드 생성 */
+    Node* SLL_CreateNode(int NewData) {
+        Node* NewNode = (Node*)malloc(sizeof(Node));
+        
+        
+        
+    }
+
+}
